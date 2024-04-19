b5a38b5702e0909896bf082aaff48511784f3486
nekalave <nekalave@gmail.com>
2024-04-19 16:17:45 +0300

    task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,18 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('task-1-button-1').addEventListener('click', function () {
-        const logItems = function (array) {
-            for (let i = 0; i < array.length; i += 1) {
-                console.log(`${i + 1} - ${array[i]}`);
+        const user = {
+            name: 'Mango',
+            age: 20,
+            hobby: 'html',
+            premium: true,
+        };
+        const updateUserInfo = function (user) {
+            user.hobby = 'skydiving';
+            user.premium = false;
+            user.mood = 'happy';
+            const keys = Object.keys(user)
+            for (const key of keys) {
+                alert(key)
+                alert(user[key])
             }
         };
-        logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong'])
-    });
-    document.getElementById('task-1-button-2').addEventListener('click', function () {
-        const logItems = function (array) {
-            for (let i = 0; i < array.length; i += 1) {
-                console.log(`${i + 1} - ${array[i]}`);
-            }
-        };
-        logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50])
+        updateUserInfo(user);
     });
 });
